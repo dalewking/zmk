6c03d30ec7183d6bd0beb04065e3b39bc17ee8c8
Dale King <dale.king@genesys.com>
2024-09-02 17:41:40 -0400

    temp

--- app/src/display/status_screen.c
+++ app/src/display/status_screen.c
@@ -61,9 +61,9 @@ lv_obj_t *zmk_display_status_screen() {
     lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_BOTTOM_LEFT, 0, 0);
 #endif
 
-#if IS_ENABLED(CONFIG_ZMK_WIDGET_WPM_STATUS)
-    zmk_widget_wpm_status_init(&wpm_status_widget, screen);
-    lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), LV_ALIGN_BOTTOM_RIGHT, 0, 0);
-#endif
+//#if IS_ENABLED(CONFIG_ZMK_WIDGET_WPM_STATUS)
+//    zmk_widget_wpm_status_init(&wpm_status_widget, screen);
+//    lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), LV_ALIGN_BOTTOM_RIGHT, 0, 0);
+//#endif
     return screen;
 }
